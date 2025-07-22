efad42024f647cb9ba5bfda9a283bfac59926b80
PiyushJ1 <piyxsh@gmail.com>
2025-07-22 16:05:00 +1000

    run prettier format

--- uploadFileToS3.ts
+++ uploadFileToS3.ts
@@ -6,20 +6,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const s3 = new AWS.S3({
-    region: process.env.AWS_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-async function uploadMediaFileToS3(filePath: string, key: string): Promise<AWS.S3.ManagedUpload.SendData> {
-    const file = fs.readFileSync(filePath);
+async function uploadMediaFileToS3(
+  filePath: string,
+  key: string,
+): Promise<AWS.S3.ManagedUpload.SendData> {
+  const file = fs.readFileSync(filePath);
 
-    const params = {
-        Bucket: process.env.AWS_S3_BUCKET_NAME!,
-        Key: key,
-        Body: file,
-        ContentType: "video/mp4",
-    };
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME!,
+    Key: key,
+    Body: file,
+    ContentType: "video/mp4",
+  };
 
-    return s3.upload(params).promise();
+  return s3.upload(params).promise();
 }
